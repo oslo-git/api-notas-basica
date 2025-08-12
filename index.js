8a0d041c676cfd62c2c44c4d829be7e593df46e5
Osliandy Sierra <osliandysierrasuarez16@gmail.com>
2025-08-12 03:51:25 -0400

    Agregando las notas iniciales

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 const express = require("express")
 const app = express()
+
+let notas = [
+    {
+        id: 1,
+        contenido: "Hola maundo 1",
+        autor:"Osliandy",
+    },
+    {
+        id: 2,
+        contenido: "Hola maundo 2",
+        autor:"Pedro",
+    },
+    {
+        id: 3,
+        contenido: "Hola maundo 3",
+        autor:"Juan",
+    },
+    {
+        id: 4,
+        contenido: "Hola maundo 4",
+        autor:"Fernando",
+    },
+]

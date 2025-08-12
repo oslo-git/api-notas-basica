971cf758e26b3b18d15e9e104396e9d80a4d4547
Osliandy Sierra <osliandysierrasuarez16@gmail.com>
2025-08-12 03:12:35 -0400

    Agregamos como dependencia de desarrollo nodemon y cambiamos el puerto al 3005

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const http = require("http")
 
+let notas = [
+    {
+        id: 1,
+        contenido: "Hola maundo 1",
+        autor:"Osliandy",
+    },
+    {
+        id: 2,
+        contenido: "Hola maundo 2",
+        autor:"Pedro",
+    },
+    {
+        id: 3,
+        contenido: "Hola maundo 3",
+        autor:"Juan",
+    },
+    {
+        id: 4,
+        contenido: "Hola maundo 4",
+        autor:"Fernando",
+    },
+]
+
 const app = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"})
-    response.end("Hola Oslaindy")
+    response.writeHead(200, {"Content-Type": "application/json"})
+    response.end(JSON.stringify(notas))
 })
 
-const PORT = 3001
+const PORT = 3005
 app.listen(PORT)
 console.log(`La API esta corriendo en el puerto: ${PORT}`)
 

886529d20c604a01dbfe475b14256923d5532f60
Osliandy Sierra <osliandysierrasuarez16@gmail.com>
2025-08-12 02:57:24 -0400

    Cambiamos el puerto al 3001, Inicanmos una lista

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const http = require("http")
 
 const app = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"})
-    response.end("Hola Muando")
+    response.end("Hola Oslaindy")
 })
 
-const PORT = 3000
+const PORT = 3001
 app.listen(PORT)
 console.log(`La API esta corriendo en el puerto: ${PORT}`)
 

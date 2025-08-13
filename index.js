720e8c9cfae92db9cc28feb2238682eeb659776f
Osliandy Sierra <osliandysierrasuarez16@gmail.com>
2025-08-12 20:37:44 -0400

    Agregamos  varios Get para acceder a una nota mediante su id y otra para devolver todas las notas

--- index.js
+++ index.js
@@ -24,6 +24,21 @@ let notas = [
     },
 ]
 
+app.get("/",(request, response, next) =>{
+    response.send("<h1>Hola Osliandy</h1>")
+})
+
+app.get("/notas", (request, response) => {
+    response.json(notas)
+})
+
+app.get("/notas/:id", (request, response) => {
+    const id = Number(request.params.id)
+    const nota = notas.find(nota => nota.id === id)
+    response.json(nota)
+})
+
 const PORT = 3005
-app.listen(PORT)
-console.log(`La API esta corriendo en el puerto: ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`La API esta corriendo en el puerto: ${PORT}`)
+})

3ad7874b149b29e679cd407a31720044a931c97f
Osliandy Sierra <osliandysierrasuarez16@gmail.com>
2025-08-12 23:39:28 -0400

    Accion post agregada

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-
+app.use(express.json())
 let notas = [
     {
         id: 1,
@@ -28,11 +28,11 @@ app.get("/",(request, response, next) =>{
     response.send("<h1>Hola Osliandy</h1>")
 })
 
-app.get("/notas", (request, response) => {
+app.get("/api/notas/", (request, response) => {
     response.json(notas)
 })
 
-app.get("/notas/:id", (request, response) => {
+app.get("/api/notas/:id", (request, response) => {
     const id = Number(request.params.id)
     const nota = notas.find(nota => nota.id === id)
     
@@ -43,10 +43,23 @@ app.get("/notas/:id", (request, response) => {
     }
 })
 
-app.delete("/notas/:id", (request, response) => {
+app.delete("/api/notas/:id", (request, response) => {
     const id = Number(request.params.id)
     notas = notas.filter((nota) => nota.id != id)
     response.status(204).end()
+})
+
+app.post("/api/notas", (request, response) => {
+    const nota = request.body
+    const nueva_nota = {
+        id: notas.length + 1,
+        constenido: nota.contenido,
+        autor: nota.autor
+    }
+    notas = [...notas,nueva_nota]
+    
+
+    response.json(nueva_nota)
 })
 
 const PORT = 3005
